89d72275251f34349e7722c5931f0256b49e3e87
Apinop <apinop.soisuwan@gmail.com>
2021-11-24 23:39:11 +0700

    two pointer!? done

--- Squares of a Sorted Array.py
+++ Squares of a Sorted Array.py
@@ -1,8 +1,14 @@
 def sortedSquares(nums):
-    for i in range(len(nums)) :
-        nums[i] = nums[i] ** 2
-    return sorted(nums)
-
+    l,n = 0,len(nums) - 1
+    p = []
+    while l <= n :
+        if nums[l] ** 2 >= nums[n] ** 2:
+            p.append(nums[l]** 2)
+            l += 1
+        else:
+            p.append(nums[n] ** 2)
+            n -= 1
+    return sorted(p)
 
 a1 = [-4,-1,0,3,10]
 #Output: [0,1,9,16,100]
